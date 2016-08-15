51cc379290ecaf570c23a097d442d986c7e1c2d9
Nobu BADAI <nobody3594@gmail.com>
2016-08-16 03:30:00 +0900

    「期待値を直接入力」の時は「現在の封印の札」を変えない

--- app/scripts/sealing-talisman2-timer/index.js
+++ app/scripts/sealing-talisman2-timer/index.js
@@ -80,7 +80,7 @@ function format(value, scale = 0) {
 }
 
 function syncCurrent() {
-  if (!syncCurrentEnabled) {
+  if (!syncCurrentEnabled || expectationInputMode === 'direct') {
     return;
   }
 
